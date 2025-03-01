299d3d12f1a35d42c131452c0ec3031009e358d2
Maciej Piechota <maciej.piechota92@gmail.com>
2025-03-01 16:45:40 +0100

    Initial Commit

---
+++ MyESCParty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyESCParty
+//
+//  Created by Maciej Piechota on 01/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyESCParty/MyESCPartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyESCPartyApp.swift
+//  MyESCParty
+//
+//  Created by Maciej Piechota on 01/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyESCPartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
