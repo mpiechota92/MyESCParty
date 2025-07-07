35fa6e91924db939c45a82dd882b55f2f9c85ac5
Maciej Piechota <maciej.piechota92@gmail.com>
2025-07-07 20:16:18 +0200

    Added toolbar item for user settings

--- MyESCParty/Views/MainTabBarView.swift
+++ MyESCParty/Views/MainTabBarView.swift
@@ -9,24 +9,49 @@ import SwiftUI
 
 struct MainTabBarView: View {
     var body: some View {
-        TabView {
-            RoomListView()
-                .tabItem {
-                    Image(systemName: "person.crop.rectangle.stack")
-                    Text("Voting rooms")
+        NavigationStack {
+            VStack {
+                HStack {
+                    Spacer()
+                    NavigationLink {
+                        Text("Asd")
+                    } label: {
+                        Image(systemName: "person.circle")
+                            .foregroundStyle(.black)
+                            .padding()
+                    }
                 }
-            ContestantsView()
-                .tabItem {
-                    Image(systemName: "flag")
-                    Text("Contestants")
+                TabView {
+                    RoomListView()
+                        .navigationTitle("Voting Rooms")
+                        .tabItem {
+                            Image(systemName: "person.crop.rectangle.stack")
+                            Text("Voting rooms")
+                        }
+                    ContestantsView()
+                        .tabItem {
+                            Image(systemName: "flag")
+                            Text("Contestants")
+                        }
+                    VoteView()
+                        .tabItem {
+                            Image(systemName: "pencil")
+                            Text("Vote")
+                        }
                 }
-            VoteView()
-                .tabItem {
-                    Image(systemName: "pencil")
-                    Text("Vote")
-                }
+            }
         }
-        
+//        .navigationBarTitleDisplayMode(.inline)
+//        .toolbar {
+//            ToolbarItemGroup(placement: .topBarTrailing) {
+//                NavigationLink {
+//                    Text("Asd")
+//                } label: {
+//                    Image(systemName: "person.circle")
+//                        .foregroundStyle(.black)
+//                }
+//            }
+//        }
     }
 }
 
