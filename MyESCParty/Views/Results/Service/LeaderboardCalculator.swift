3e4ccace4bfee934668bf5f779712f6f02790a5c
Maciej Piechota <maciej.piechota92@gmail.com>
2025-07-28 15:53:34 +0200

    Code cleanup

--- MyESCParty/Views/Results/Service/LeaderboardCalculator.swift
+++ MyESCParty/Views/Results/Service/LeaderboardCalculator.swift
@@ -8,31 +8,6 @@
 import Foundation
 
 struct LeaderboardCalculator {
-    typealias Leaderboard = [LeaderboardEntry]
-    
-    let contestants: [Contestant]
-    
-    
-    
-    private func getResultFromVote(vote: Vote) -> Leaderboard {
-        let sortedRanking = vote.ranking.sorted { $0.key < $1.key }
-        let sortedList = sortedRanking.compactMap { _, contestantId in
-            return contestants.first { $0.id == contestantId }
-        }
-        
-        let points: [Int] = [12, 10, 8, 7, 6, 5, 4, 3, 2, 1]
-        
-        let voteResult: [LeaderboardEntry] = sortedList.enumerated().map { index, contestant in
-            guard index < 10 else {
-                return LeaderboardEntry(contestant: contestant, score: 0)
-            }
-            
-            return LeaderboardEntry(contestant: contestant, score: points[index])
-        }
-        
-        return voteResult
-    }
-    
     static func calculate(from votes: [Vote], contestants: [Contestant]) -> [LeaderboardEntry] {
         let contestantsMap = Dictionary(uniqueKeysWithValues: contestants.map { ($0.id, $0) })
         let points: [Int] = [12, 10, 8, 7, 6, 5, 4, 3, 2, 1]
