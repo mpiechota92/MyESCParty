29a1f2639eac12f5da0d58a8932b8d5b6ecd0e05
Maciej Piechota <maciej.piechota92@gmail.com>
2026-04-02 14:52:57 +0200

    Add image cache to load/save images from memory/disk

---
+++ MyESCParty/Core/Data/ImageCache.swift
@@ -0,0 +1,44 @@
+//
+//  ImageCache.swift
+//  MyESCParty
+//
+//  Created by Maciej Piechota on 02/04/2026.
+//
+
+import Foundation
+import CryptoKit
+
+final class ImageCache {
+    static let shared = ImageCache()
+    
+    private let memory = ImageMemoryCache()
+    private let disk = ImageDiskCache()
+    
+    func image(for imageUrl: String) -> Data? {
+        let memoryData = memory.get(for: imageUrl)
+        if let memoryData {
+            return memoryData
+        }
+        
+        let diskData = disk.load(for: cacheKey(for: imageUrl))
+        if let diskData {
+            memory.set(diskData, for: imageUrl)
+            return diskData
+        }
+        
+        return nil
+    }
+    
+    func saveImage(_ data: Data, fileName key: String) {
+        memory.set(data, for: key)
+        disk.save(data, for: key)
+    }
+    
+    private func cacheKey(for url: String) -> String {
+        // Change the url to static 32bytes length to avoid too long
+        // names while saving on disk
+        let data = Data(url.utf8)
+        let hash = SHA256.hash(data: data)
+        return hash.map { String(format: "%02x", $0) }.joined()
+    }
+}
